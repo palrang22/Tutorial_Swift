a91ad8d05db02a36b33a5b1d23bd13b7648980db
palrang22 <https://github.com/palrang22>
2023-05-15 15:46:22 +0900

    Initial Commit

---
+++ Tutorial_Swift/Tutorial_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_SwiftApp.swift
+//  Tutorial_Swift
+//
+//  Created by 김승희 on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tutorial_SwiftUITests/Tutorial_SwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tutorial_SwiftUITestsLaunchTests.swift
+//  Tutorial_SwiftUITests
+//
+//  Created by 김승희 on 2023/05/15.
+//
+
+import XCTest
+
+final class Tutorial_SwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
